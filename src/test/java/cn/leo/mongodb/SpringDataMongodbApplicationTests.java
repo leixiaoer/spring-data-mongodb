e4f6987cf3f8f772d2920213960ca0608c2995ca
leo <little_leo@163.com>
2022-11-21 23:51:56 +0800

    Initial commit

---
+++ src/test/java/cn/leo/mongodb/SpringDataMongodbApplicationTests.java
@@ -0,0 +1,27 @@
+package cn.leo.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+class SpringDataMongodbApplicationTests {
+
+    @Resource
+    MongoTemplate mongoTemplate;
+
+    //测试创建集合
+    @Test
+    public void testCollection() {
+        boolean exists = mongoTemplate.collectionExists("emp");
+        if (exists) {
+            // 删除集合
+            mongoTemplate.dropCollection("emp");
+        }
+        //创建集合
+        mongoTemplate.createCollection("emp");
+    }
+
+}
